3b85d5defade03913e08bd5cdebfc931abe4af36
Noel Renderos <noel.renderos@mh.gob.sv>
2024-04-09 11:09:31 -0600

    Se reestructuro la clase Puesto utilizando la clase Servicio

--- src/main/java/com/mh/consultatelefonica/controller/PuestoController.java
+++ src/main/java/com/mh/consultatelefonica/controller/PuestoController.java
@@ -4,15 +4,8 @@
  */
 package com.mh.consultatelefonica.controller;
 
-import com.mh.consultatelefonica.exception.PuestoNotFoundException;
-import com.mh.consultatelefonica.exception.RolNotFoundException;
-import com.mh.consultatelefonica.exception.UnidadNotFoundException;
 import com.mh.consultatelefonica.model.Puesto;
-import com.mh.consultatelefonica.model.Rol;
-import com.mh.consultatelefonica.model.Unidad;
-import com.mh.consultatelefonica.repository.PuestoRepository;
-import com.mh.consultatelefonica.repository.RolRepository;
-import com.mh.consultatelefonica.repository.UnidadRepository;
+import com.mh.consultatelefonica.service.PuestoService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -30,64 +23,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PuestoController {
     
-    @Autowired
-    private RolRepository rolRepository;
-    
-    @Autowired
-    private UnidadRepository unidadRepository;
-    
     @Autowired 
-    private PuestoRepository puestoRepository;
+    private PuestoService puestoService;
     
     @PostMapping("/puesto/{rolId}/{unidadId}")
-    Puesto newPuesto(@RequestBody Puesto newPuesto, @PathVariable("rolId") Long rolId, @PathVariable("unidadId") Long unidadId){
-        Rol findRol = rolRepository.findById(rolId)
-                .orElseThrow(() -> new RolNotFoundException(rolId));
-        Unidad findUnidad = unidadRepository.findById(unidadId)
-                .orElseThrow(() -> new UnidadNotFoundException(unidadId));
-        newPuesto.setRol(findRol);
-        newPuesto.setUnidad(findUnidad);
-        
-        return puestoRepository.save(newPuesto);
+    public Puesto newPuesto(@RequestBody Puesto newPuesto, @PathVariable("rolId") Long rolId, @PathVariable("unidadId") Long unidadId){
+        return puestoService.savePuesto(newPuesto, rolId, unidadId);
     }
     
     @GetMapping("/puestos")
-    List<Puesto> getPuestos(){
-        return puestoRepository.findAll();
+    public List<Puesto> getPuestos(){
+        return puestoService.getPuestos();
     }
     
     @GetMapping("/puesto/{id}")
-    Puesto getPuestoById(@PathVariable Long id){
-        return puestoRepository.findById(id)
-                .orElseThrow(() -> new PuestoNotFoundException(id));
+    public Puesto getPuestoById(@PathVariable Long id){
+        return puestoService.getPuestoById(id);
     }
     
     @PutMapping("/puesto/{rolId}/{unidadId}/{id}")
-    Puesto updatePuesto(@RequestBody Puesto newPuesto, @PathVariable("rolId") Long rolId, @PathVariable("unidadId") Long unidadId, @PathVariable("id") Long id){
-        Rol findRol = rolRepository.findById(rolId)
-                .orElseThrow(() -> new RolNotFoundException(rolId));
-        Unidad findUnidad = unidadRepository.findById(unidadId)
-                .orElseThrow(() -> new UnidadNotFoundException(unidadId));
-        
-        return puestoRepository.findById(id)
-                .map(puesto -> {
-                    puesto.setName(newPuesto.getName());
-                    puesto.setSalary(newPuesto.getSalary());
-                    puesto.setRol(findRol);
-                    puesto.setUnidad(findUnidad);
-                    
-                    return puestoRepository.save(puesto);
-                })
-                .orElseThrow(() -> new PuestoNotFoundException(id));
+    public Puesto updatePuesto(@RequestBody Puesto newPuesto, @PathVariable("rolId") Long rolId, @PathVariable("unidadId") Long unidadId, @PathVariable("id") Long id){
+        return puestoService.updatePuesto(newPuesto, rolId, unidadId, id);
     }
     
     @DeleteMapping("/puesto/{id}")
-    String deletePuesto(@PathVariable Long id){
-        if(!puestoRepository.existsById(id)){
-            throw new PuestoNotFoundException(id);
-        }
-        puestoRepository.deleteById(id);
-        
-        return "Puesto with id " + id + " has been succesfully deleted";
+    public String deletePuesto(@PathVariable Long id){
+        return puestoService.deletePuesto(id);
     }
 }
---
+++ src/main/java/com/mh/consultatelefonica/service/PuestoService.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mh.consultatelefonica.service;
+
+import com.mh.consultatelefonica.exception.PuestoNotFoundException;
+import com.mh.consultatelefonica.model.Puesto;
+import com.mh.consultatelefonica.model.Rol;
+import com.mh.consultatelefonica.model.Unidad;
+import com.mh.consultatelefonica.repository.PuestoRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author noel.renderos
+ */
+@Service
+public class PuestoService {
+    
+    @Autowired
+    private RolService rolService;
+    
+    @Autowired
+    private UnidadService unidadService;
+    
+    @Autowired 
+    private PuestoRepository puestoRepository;
+    
+    public Puesto savePuesto(Puesto newPuesto, Long rolId, Long unidadId){
+        Rol findRol = rolService.getRolById(rolId);
+        Unidad findUnidad = unidadService.getUnidadById(unidadId);
+        newPuesto.setRol(findRol);
+        newPuesto.setUnidad(findUnidad);
+        
+        return puestoRepository.save(newPuesto);
+    }
+    
+    public List<Puesto> getPuestos(){
+        return puestoRepository.findAll();
+    }
+    
+    public Puesto getPuestoById(Long id){
+        return puestoRepository.findById(id)
+                .orElseThrow(() -> new PuestoNotFoundException(id));
+    }
+    
+    public Puesto updatePuesto(Puesto newPuesto, Long rolId, Long unidadId, Long id){
+        Rol findRol = rolService.getRolById(rolId);
+        Unidad findUnidad = unidadService.getUnidadById(unidadId);
+        
+        return puestoRepository.findById(id)
+                .map(puesto -> {
+                    puesto.setName(newPuesto.getName());
+                    puesto.setSalary(newPuesto.getSalary());
+                    puesto.setRol(findRol);
+                    puesto.setUnidad(findUnidad);
+                    
+                    return puestoRepository.save(puesto);
+                })
+                .orElseThrow(() -> new PuestoNotFoundException(id));
+    }
+    
+    public String deletePuesto(Long id){
+        if(!puestoRepository.existsById(id)){
+            throw new PuestoNotFoundException(id);
+        }
+        puestoRepository.deleteById(id);
+        
+        return "Puesto with id " + id + " has been succesfully deleted";
+    }
+}
